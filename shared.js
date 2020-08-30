c006c6b5183768370926b37e97db139079954f40
altaftelus <mdaltaf.ansari@kinesso.com>
2020-08-30 12:50:44 +0530

    dynamically add/remove class using classList

--- shared.js
+++ shared.js
@@ -5,25 +5,35 @@ var selectPlanButtons = document.querySelectorAll('.plan button');
 var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
 
+modalNoButton.addEventListener('click', closeModal);
 backdrop.addEventListener('click', function () {
-  mobileNav.style.display = 'none';
+  // mobileNav.style.display = 'none';
+  mobileNav.classList.remove('open')
   closeModal();
 });
-modalNoButton.addEventListener('click', closeModal);
+
 
 function closeModal() {
-  backdrop.style.display = 'none';
-  modal.style.display = 'none';
+  // backdrop.style.display = 'none';
+  // modal.style.display = 'none';
+  modal.classList.remove('open');
+  backdrop.classList.remove('open');
 }
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener('click', function () {
-    modal.style.display = 'block';
-    backdrop.style.display = 'block';
+    // modal.style.display = 'block';
+    // backdrop.style.display = 'block';
+    // modal.className = 'open'; //This will actually overwrite the complete class list
+    // backdrop.className = 'open'; //This will actually overwrite the complete class list
+    modal.classList.add('open');
+    backdrop.classList.add('open');
   });
 }
 
 toggleButton.addEventListener('click', function () {
-  mobileNav.style.display = 'block';
-  backdrop.style.display='block'
+  // mobileNav.style.display = 'block';
+  // backdrop.style.display = 'block';
+  mobileNav.classList.add('open')
+  backdrop.classList.add('open')
 });
