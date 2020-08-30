135420d9aadace83eeb081e8f00b72706ba55a78
altaftelus <mdaltaf.ansari@kinesso.com>
2020-08-30 11:55:40 +0530

    modal added

---
+++ shared.js
@@ -0,0 +1,14 @@
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+
+backdrop.addEventListener('click', function () {
+  backdrop.style.display = 'none';
+  modal.style.display='none'
+});
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener('click', function () {
+    modal.style.display = 'block';
+    backdrop.style.display = 'block';
+  });
+}
